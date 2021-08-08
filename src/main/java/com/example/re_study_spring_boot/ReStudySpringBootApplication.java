f6776b7032ccb0f94c5b5ce7914f9e73c4fa29f9
Won-young <dnjsdud2257@gmail.com>
2021-08-08 17:04:24 +0900

    Initial commit

---
+++ src/main/java/com/example/re_study_spring_boot/ReStudySpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.re_study_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReStudySpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReStudySpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/re_study_spring_boot/ReStudySpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.re_study_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReStudySpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
